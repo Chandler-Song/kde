8998a8e9a598671c1b5e199e18b93b8fce858f52
Like Xu <like.xu@intel.com>
2018-10-11 15:05:55 +0000

    modify kernel_mailing_list/git_am_using_reset_hard.sh with more try when git am fails in first time-meetable commit

--- kernel_mailing_list/git_am_using_reset_hard.sh
+++ kernel_mailing_list/git_am_using_reset_hard.sh
@@ -10,12 +10,24 @@ do
             DATE=`date -d "${day}" "+%Y-%m-%d"`
             branch_name=`echo ${target_patch} | sed "s/\[//g" | sed "s/\]//g" | \
                     sed "s/://g" | sed "s/\///g" | sed "s/ /_/g" | sed "s/\./_/g"`   
-            git checkout -b ${branch_name} `git log --before="${DATE}" | grep "commit " | head -n1 | awk '{ print $2 }'`
-            echo `git log | grep "commit " | head -n1 | awk '{ print $2 }'` > .latest_commit
+            git checkout -b ${branch_name} master
+            for i in `git log --before="${DATE}" | grep "commit " | head -n10 | awk '{ print $2 }'`
+            do
+                echo ${i}
+                git reset --hard ${i}
+                git am ${target_patch}
+                if [ ! -f ".git/rebase-apply/patch" ]; then
+                    echo "git am SUCCESS on commit ${i}"
+                    exit
+                fi
+                echo "git am FAILED on commit ${i}"
+                git am --abort
+            done
             git am ${target_patch}
             ;;
         r)
             echo "The arg is : $OPTARG" #参数存在$OPTARG中
+            git am --abort
             target_patch=$OPTARG
             day=`cat ${target_patch} | grep "Date" | head -n1 | sed "s/Date: //g"`
             DATE=`date -d "${day}" "+%Y-%m-%d"`
@@ -23,7 +35,6 @@ do
                     sed "s/://g" | sed "s/\///g" | sed "s/ /_/g" | sed "s/\./_/g"`
             git checkout master
             git branch -D ${branch_name}
-            rm .latest_commit
             ;;
         ?)  #当有不认识的选项的时候arg为?
         echo "unkonw argument"
