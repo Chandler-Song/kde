986e11bcc80dc6fd9f017b335ac32b4ac8371894
Aaron Qubit <like.xu@intel.com>
2018-10-26 22:03:00 +0800

    fix kvm_set_memory_region

--- shells/get_functions.sh
+++ shells/get_functions.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-FILE=$1
+F=$1
+FILE=$1.tmp
 OUTPUT=`echo $1 | sed 's/\//_/g'`
 echo "" > ${OUTPUT}.h
+cat ${F} | grep "EXPORT_SYMBOL_GPL" >> ${OUTPUT}.h
+cat ${F} | grep -v "#if" > ${FILE}
 cat ${FILE} | grep -n "^{" | awk -F ":" '{ print $1 }' > lines
 while read line
 do
@@ -13,14 +16,17 @@ do
 i=`expr ${i} - 1`
 cond=`sed -n "${i},${i}p" ${FILE} | grep "^$" | wc -l`
 done
-sed -n "${i},${line}p" ${FILE} | grep -v "{" | grep -v "^$">> ${OUTPUT}
+sed -n "${i},${line}p" ${FILE} | grep -v "{" | grep -v "^$" >> ${OUTPUT}
 done < lines
 
 # macos x
-sh remove_comment.sh ${OUTPUT} | tr "\t" " " | tr "\n" " " | tr " " "#" | \
-    sed 's/##//g' | sed 's/#/ /g' | sed 's/,/ /g' | \
-    sed 's/  / /g' | sed -e 's/)/);\'$'\n/g' | sed 's/^ //g' > ${OUTPUT}.h
+echo "\n" >> ${OUTPUT}.h
+sh $HUB/kde/shells/remove_comment.sh ${OUTPUT} | tr "\t" " " | tr "\n" " " | tr " " "#" | \
+    sed 's/##//g' | sed 's/#/ /g' | sed 's/,/, /g' | \
+    sed 's/  / /g' | sed -e 's/)/)\'$'\n/g' | sed 's/^ //g' >> ${OUTPUT}.h
 
 rm ${OUTPUT}
+rm lines
+rm ${FILE}
 
 # linux: cat arch_x86_kvm_cpuid.c.h | tr "\n" " " | tr "\t" " " | tr " " "#" | sed 's/##//g'  |  sed 's/#/ /g' | sed 's/  //g' |sed 's/,/, /g'|sed 's/  / /g'|sed 's/)/);\n/g'
