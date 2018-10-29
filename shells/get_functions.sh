92ffff29cadb1a00ffd58ec848b15db1696a1a70
Like Xu <like.xu@intel.com>
2018-10-29 08:56:37 +0000

    update shells/get_functions.sh for linux machine

--- shells/get_functions.sh
+++ shells/get_functions.sh
@@ -5,6 +5,7 @@ FILE=$1.tmp
 OUTPUT=`echo $1 | sed 's/\//_/g'`
 echo "" > ${OUTPUT}.h
 cat ${F} | grep "EXPORT_SYMBOL_GPL" >> ${OUTPUT}.h
+echo "\n" >> ${OUTPUT}.h
 cat ${F} | grep -v "#if" > ${FILE}
 cat ${FILE} | grep -n "^{" | awk -F ":" '{ print $1 }' > lines
 while read line
@@ -20,10 +21,18 @@ sed -n "${i},${line}p" ${FILE} | grep -v "{" | grep -v "^$" >> ${OUTPUT}
 done < lines
 
 # macos x
-echo "\n" >> ${OUTPUT}.h
-sh $HUB/kde/shells/remove_comment.sh ${OUTPUT} | tr "\t" " " | tr "\n" " " | tr " " "#" | \
+# echo "\n" >> ${OUTPUT}.h
+# sh $HUB/kde/shells/remove_comment.sh ${OUTPUT} | tr "\t" " " | tr "\n" " " | tr " " "#" | \
+#     sed 's/##//g' | sed 's/#/ /g' | sed 's/,/, /g' | \
+#     sed 's/  / /g' | sed -e 's/)/)\'$'\n/g' | sed 's/^ //g' >> ${OUTPUT}.h
+
+# linux
+sh /root/kde/shells/remove_comment.sh ${OUTPUT} | tr "\t" " " | tr "\n" " " | tr " " "#" | \
     sed 's/##//g' | sed 's/#/ /g' | sed 's/,/, /g' | \
-    sed 's/  / /g' | sed -e 's/)/)\'$'\n/g' | sed 's/^ //g' >> ${OUTPUT}.h
+    sed 's/  / /g' | sed -e 's/)/)\n/g' | sed 's/^ //g' >> ${OUTPUT}.h
+
+echo "\n" >> ${OUTPUT}.h
+cat ${OUTPUT}.h | grep -o "(.*)" | tr "," "\n" | sed 's/(//g' | sed 's/)//g' | sed 's/^ //g' | sort | uniq -c| sort -nr >> ${OUTPUT}.h
 
 rm ${OUTPUT}
 rm lines
